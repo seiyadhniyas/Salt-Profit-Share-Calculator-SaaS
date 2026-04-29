f9b7c38d0c069bb9229a1fd66eaae8987aee61b2
Fixmation <seiyadhniyas@gmail.com>
2026-04-29 08:41:17 +0530

    Fix: update server configuration to disable auto-open and use 127.0.0.1 for HMR

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
+    open: false,
     host: '0.0.0.0',
     hmr: {
       protocol: 'ws',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3000
     }
   },
